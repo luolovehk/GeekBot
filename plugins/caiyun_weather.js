979a14dba3ca5c8704c763f84d1335889f02bab9
luolovehk <61071476+luolovehk@users.noreply.github.com>
2020-07-20 22:18:42 +0800

    Update caiyun_weather.js

--- plugins/caiyun_weather.js
+++ plugins/caiyun_weather.js
@@ -43,9 +43,9 @@ class Plugin extends Bot {
     }
     await this.sendMarkdown(`
 ${addr || ''} 🌤 天气预报 
-> ⭐ 实时气温 ${data.result.realtime.temperature+' ℃'}
-> ⬆ 最高气温 ${data.result.daily.temperature[0].max+' ℃'}
-> ⬇ 最低气温 ${data.result.daily.temperature[0].min+' ℃'}
+> ⭐ 实时气温 ${data.result.realtime.temperature+'℃'}
+> ⬆ 最高气温 ${data.result.daily.temperature[0].max+'℃'}
+> ⬇ 最低气温 ${data.result.daily.temperature[0].min+'℃'}
 
 **降雨提醒：**
 > ${data.result.minutely.description.trim()}
