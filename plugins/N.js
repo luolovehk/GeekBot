35e0cb41637f9041e198f72468ce689087d58c11
luolovehk <61071476+luolovehk@users.noreply.github.com>
2020-07-20 13:41:32 +0800

    Update N.js

--- plugins/N.js
+++ plugins/N.js
@@ -16,7 +16,7 @@ class Plugin extends Bot {
 
   run () {
     axios.get(this.API + '?key=' + this.API_KEY).then(res => {
-      const c = (`> 🌺晚安 |🌺 <\n\n${res.data.newslist[0].content}`\n\n${res.data.newslist[0].mrname}`);
+      const c = (`> 🌺晚安 |🌺 <\n\n${res.data.newslist[0].content}\n\n${res.data.newslist[0].mrname}`);
       this.sendText(c);
     });
   }
