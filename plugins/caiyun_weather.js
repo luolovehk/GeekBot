f7afbcb063cbfaa4b813541e158fd606c799d152
luolovehk <61071476+luolovehk@users.noreply.github.com>
2020-07-20 21:18:39 +0800

    Update caiyun_weather.js

--- plugins/caiyun_weather.js
+++ plugins/caiyun_weather.js
@@ -43,7 +43,7 @@ class Plugin extends Bot {
     }
     await this.sendMarkdown(`
 ${addr || ''} 🌤天气预报 
-> ${data.result.hourly.temperature[0].value}
+>  ${data.result.daily.temperature[avg]}
 
 **降雨提醒：**
 > ${data.result.minutely.description.trim()}
